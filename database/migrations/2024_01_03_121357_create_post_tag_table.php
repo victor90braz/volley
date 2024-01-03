0042771f8375dfd5190ca98c8cdd04c46cfd6594
victor braz <victor.90braz@gmail.com>
2024-01-03 17:01:04 +0100

    fix error sintaxis

--- database/migrations/2024_01_03_121357_create_post_tag_table.php
+++ database/migrations/2024_01_03_121357_create_post_tag_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedInteger('post_id');
-            $table->unsignedInteger('tag_id');
+            $table->primary(['post_id', 'tag_id']);
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
 
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
