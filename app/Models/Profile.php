1a00aeece2faeccd0ae926e36f4096132880baea
victor braz <victor.90braz@gmail.com>
2024-01-03 12:34:33 +0100

    if i have a profile i belongsTo from the user

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    // todo - if i have a profile it's belongsTo from the user
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
